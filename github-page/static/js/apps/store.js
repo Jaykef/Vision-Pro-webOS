c7183b4be31f94b4e353ba81a5af495b8afd860a
Jaward Sesay <jkef96@icloud.com>
2024-02-16 22:18:16 +0800

    for github page

---
+++ github-page/static/js/apps/store.js
@@ -0,0 +1,80 @@
+
+function appContent() {
+    return `
+        <div class="store-content">
+            <div class="hero">
+                <h3 style="position:absolute; color:#fff;margin:30px" >Apps & Games</h3>
+                <img src="./static/img/for-all-mankind.png" alt="Placeholder image">
+            </div>
+            <div class="cards" style="margin:-120px 30px 0px 30px">
+                <h5 style="position:absolute; color:#fff; top:350px">Get Started ></h5>
+                <div class="row">
+                    <img src="./static/img/for-all-mankind.png" alt="./static/video/fam.mp4">
+                </div>
+                <div class="row">
+                    <img src="./static/img/tedlasso.png" alt="./static/video/fam.mp4">
+                </div>
+                <div class="row">
+                    <img src="./static/img/monarch.png" alt="./static/video/fam.mp4">
+                </div>
+                <div class="row">
+                    <img src="./static/img/hground.png" alt="./static/video/fam.mp4">
+                </div>
+                <div class="row">
+                    <img src="./static/img/kflower.png" alt="./static/video/fam.mp4" >
+                </div>
+                <div class="row">
+                    <img src="./static/img/family.png" alt="./static/video/fam.mp4">
+                </div>
+                <div class="row">
+                    <img src="./static/img/dynasty.png" alt="./static/video/fam.mp4">
+                </div>
+                <div class="row">
+                    <img src="./static/img/invasion.png" alt="./static/video/fam.mp4">
+                </div>
+                <div class="row">
+                    <img src="./static/img/foundation.png" alt="./static/video/fam.mp4">
+                </div>
+                <div class="row">
+                    <img src="./static/img/moa.png" alt="./static/video/fam.mp4" >
+                </div>
+            </div>
+        </div>
+    `;
+}
+
+function appControls(){
+    return `
+        <div class="store-sidebar">
+
+        <div class="selected">
+            <svg  xmlns="http://www.w3.org/2000/svg" width="95" height="84" fill="none" viewBox="0 0 95 84">
+            <path fill="none" fill-rule="evenodd" d="m58.651 27.556.024.04-.024-.04Zm.912 36.928-.002.005.004-.005h-.002Z" clip-rule="evenodd" />
+            <path fill="currentColor" d="M0 58.584c0 3.242 2.653 5.9 5.9 5.9h53.663c2.592-4.81-.745-11.796-6.7-11.796h-18.32l25.31-43.842c1.624-2.8.68-6.43-2.152-8.053-2.8-1.624-6.43-.68-8.053 2.153l-2.624 4.54-2.625-4.54C42.78.146 39.18-.831 36.346.793c-2.799 1.62-3.776 5.22-2.152 8.053l6.046 10.441-19.29 33.396H5.9c-3.242 0-5.9 2.658-5.9 5.9Zm18.588 21.798c-1.623 2.832-5.253 3.776-8.052 2.152-2.832-1.623-3.777-5.253-2.153-8.052l4.22-7.288c4.749-1.445 8.643-.326 11.683 3.36l-5.698 9.828Z" />
+            <path fill="currentColor" fill-rule="evenodd" d="M59.565 64.484h-.002l-.002.005.004-.005Z" clip-rule="evenodd" />
+            <path fill="currentColor" d="M88.506 64.484h-8.558l5.782 10.002c1.624 2.832.647 6.434-2.152 8.053-2.833 1.624-6.434.647-8.053-2.152-4.276-7.396-8.077-13.984-11.408-19.757l-.007-.011a7940.194 7940.194 0 0 0-10.417-18.023c-4.928-8.558-1.416-17.111 2.096-20l2.858 4.953.004.008.024.04 1.407 2.44a26119.19 26119.19 0 0 0 13.083 22.651h15.34c3.248 0 5.901 2.657 5.901 5.9a5.896 5.896 0 0 1-5.9 5.896Z" />
+            </svg>
+        </div>
+
+            <svg fill="#ffffff82" viewBox="0 0 1920 1920" xmlns="http://www.w3.org/2000/svg" transform="matrix(-1, 0, 0, 1, 0, 0)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M735.385 336.094c218.24 0 395.977 177.624 395.977 395.976v113.137c0 121.96-56.568 229.78-143.57 302.526 94.13 13.916 187.354 34.959 278.315 64.6 122.414 39.825 204.664 155.676 204.664 288.159v200.364l-26.814 16.63c-148.434 92.32-392.017 202.515-708.572 202.515-174.795 0-439.76-35.186-708.685-202.514L0 1700.856v-189.39c0-140.629 89.264-263.042 221.973-304.79 85.418-26.7 172.533-46.498 260.327-59.509-86.55-72.746-142.891-180.339-142.891-301.96V732.07c0-218.352 177.623-395.976 395.976-395.976ZM1183.405 0c218.24 0 395.976 177.624 395.976 395.977v113.136c0 121.96-56.568 229.893-143.57 302.526 94.13 13.916 187.241 35.072 278.316 64.6 122.413 40.051 204.663 155.79 204.663 288.272v200.364l-26.7 16.631c-77.612 48.31-181.81 101.03-308.183 140.742v-21.723c0-181.696-113.589-340.766-282.727-395.75a1720.133 1720.133 0 0 0-111.553-32.244c35.751-69.805 54.871-147.416 54.871-227.29V732.104c0-250.483-182.036-457.975-420.414-500.175C886.762 95.487 1023.656 0 1183.404 0Z" fill-rule="evenodd"></path> </g></svg>
+
+            
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50" viewBox="0,0,256,256"
+            style="fill:#000000;">
+                <g fill="#ffffff82" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(5.12,5.12)"><path d="M21,3c-9.39844,0 -17,7.60156 -17,17c0,9.39844 7.60156,17 17,17c3.35547,0 6.46094,-0.98437 9.09375,-2.65625l12.28125,12.28125l4.25,-4.25l-12.125,-12.09375c2.17969,-2.85937 3.5,-6.40234 3.5,-10.28125c0,-9.39844 -7.60156,-17 -17,-17zM21,7c7.19922,0 13,5.80078 13,13c0,7.19922 -5.80078,13 -13,13c-7.19922,0 -13,-5.80078 -13,-13c0,-7.19922 5.80078,-13 13,-13z"></path></g></g>
+            </svg>
+
+        </div>
+    `;
+}
+
+function bottomNav(){ 
+    return`
+        <div class="store-controls">
+        </div>
+    `
+}
+
+appContent();
+appControls();
+bottomNav();
---
+++ github-page/static/js/panoramic.js
@@ -0,0 +1,97 @@
+import * as THREE from "three";
+import { OrbitControls } from '../libs/Threejs/OrbitControls.js';
+const panoramasArray = ["./app/static/img/livingroom_3.jpg"];
+let panoramaNumber = Math.floor(Math.random() * panoramasArray.length);
+
+
+let scene;
+let renderer;
+let camera, controls;
+let windowWidth = window.innerWidth;
+let windowHeight = window.innerHeight;
+let lon = 0;
+let lat = 0;
+let phi = 0;
+let theta = 0;
+const dpr = window.devicePixelRatio;
+let sphereMaterial;
+let isUserInteracting = false;
+
+init();
+animate();
+
+function init() {
+    const container = document.getElementById("parallax");
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(windowWidth, windowHeight);
+    renderer.setPixelRatio(dpr);
+    container.appendChild(renderer.domElement);
+
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 0.1, 100 );
+    camera.target = new THREE.Vector3(0, 0, 0);
+    camera.position.z = 0.01;
+
+    controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableZoom = false;
+    controls.enablePan = false;
+    controls.enableRotate = true;
+
+    createSphere();
+    window.addEventListener( 'resize', onWindowResize );
+}
+
+function createSphere() {
+    const sphere = new THREE.SphereGeometry(100, 100, 40);
+    sphere.applyMatrix4(new THREE.Matrix4().makeScale(-1, 1, 1));
+    sphereMaterial = new THREE.MeshBasicMaterial();
+    sphereMaterial.map = new THREE.TextureLoader().load(
+        panoramasArray[panoramaNumber]
+    );
+
+    const sphereMesh = new THREE.Mesh(sphere, sphereMaterial);
+    scene.add(sphereMesh);
+}
+
+
+function animate() {
+    requestAnimationFrame(animate);
+  
+    if (controls.enabled)
+
+    if ( isUserInteracting === false ) {
+
+        controls.update();
+        lon += 0.01;
+    }
+
+    lat = Math.max(200, Math.min(230, lat ) );
+    phi = THREE.MathUtils.degToRad( 110 - lat );
+    theta = THREE.MathUtils.degToRad( lon );
+
+    const x = 500 * Math.sin( phi ) * Math.cos( theta );
+    const y = 500 * Math.cos( phi );
+    const z = 500 * Math.sin( phi ) * Math.sin( theta );
+
+    camera.lookAt( x, y, z );
+    renderer.render( scene, camera );
+    render();  
+}
+
+
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+
+function render() {
+  renderer.render(scene, camera);
+}
+
+
