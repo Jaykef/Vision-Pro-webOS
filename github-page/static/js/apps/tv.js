21d055d8bdab1d45c6ef45fc4e85331a166a660a
Jaward Sesay <jkef96@icloud.com>
2024-02-16 22:43:49 +0800

    correct path

--- github-page/static/js/apps/tv.js
+++ github-page/static/js/apps/tv.js
@@ -7,39 +7,39 @@ function appContent() {
                     <svg viewBox="-1.5 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#ffffff" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>apple [#ffffff]</title> <desc>Created with Sketch.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Dribbble-Light-Preview" transform="translate(-102.000000, -7439.000000)" fill="#ffffff"> <g id="icons" transform="translate(56.000000, 160.000000)"> <path d="M57.5708873,7282.19296 C58.2999598,7281.34797 58.7914012,7280.17098 58.6569121,7279 C57.6062792,7279.04 56.3352055,7279.67099 55.5818643,7280.51498 C54.905374,7281.26397 54.3148354,7282.46095 54.4735932,7283.60894 C55.6455696,7283.69593 56.8418148,7283.03894 57.5708873,7282.19296 M60.1989864,7289.62485 C60.2283111,7292.65181 62.9696641,7293.65879 63,7293.67179 C62.9777537,7293.74279 62.562152,7295.10677 61.5560117,7296.51675 C60.6853718,7297.73474 59.7823735,7298.94772 58.3596204,7298.97372 C56.9621472,7298.99872 56.5121648,7298.17973 54.9134635,7298.17973 C53.3157735,7298.17973 52.8162425,7298.94772 51.4935978,7298.99872 C50.1203933,7299.04772 49.0738052,7297.68074 48.197098,7296.46676 C46.4032359,7293.98379 45.0330649,7289.44985 46.8734421,7286.3899 C47.7875635,7284.87092 49.4206455,7283.90793 51.1942837,7283.88393 C52.5422083,7283.85893 53.8153044,7284.75292 54.6394294,7284.75292 C55.4635543,7284.75292 57.0106846,7283.67793 58.6366882,7283.83593 C59.3172232,7283.86293 61.2283842,7284.09893 62.4549652,7285.8199 C62.355868,7285.8789 60.1747177,7287.09489 60.1989864,7289.62485" id="apple-[#ffffff]"> </path> </g> </g> </g> </g></svg>
                     <h3> TV+</h3>
                 </div>
-                <img src="./static/img/mname.png" alt="Placeholder image" style="position: absolute;height: 50px;width: 160px;margin: 300px 0px 0px 15px">
-                <img src="./static/img/for-all-mankind.png" alt="Placeholder image">
+                <img src="./github-page/static/img/mname.png" alt="Placeholder image" style="position: absolute;height: 50px;width: 160px;margin: 300px 0px 0px 15px">
+                <img src="./github-page/static/img/for-all-mankind.png" alt="Placeholder image">
             </div>
             <div class="cards">
                 <div class="row">
-                    <img src="./static/img/for-all-mankind.png" alt="./static/video/fam.mp4">
+                    <img src="./github-page/static/img/for-all-mankind.png" alt="./github-page/static/video/fam.mp4">
                 </div>
                 <div class="row">
-                    <img src="./static/img/tedlasso.png" alt="./static/video/fam.mp4">
+                    <img src="./github-page/static/img/tedlasso.png" alt="./github-page/static/video/fam.mp4">
                 </div>
                 <div class="row">
-                    <img src="./static/img/monarch.png" alt="./static/video/fam.mp4">
+                    <img src="./github-page/static/img/monarch.png" alt="./github-page/static/video/fam.mp4">
                 </div>
                 <div class="row">
-                    <img src="./static/img/hground.png" alt="./static/video/fam.mp4">
+                    <img src="./github-page/static/img/hground.png" alt="./github-page/static/video/fam.mp4">
                 </div>
                 <div class="row">
-                    <img src="./static/img/kflower.png" alt="./static/video/fam.mp4" >
+                    <img src="./github-page/static/img/kflower.png" alt="./github-page/static/video/fam.mp4" >
                 </div>
                 <div class="row">
-                    <img src="./static/img/family.png" alt="./static/video/fam.mp4">
+                    <img src="./github-page/static/img/family.png" alt="./github-page/static/video/fam.mp4">
                 </div>
                 <div class="row">
-                    <img src="./static/img/dynasty.png" alt="./static/video/fam.mp4">
+                    <img src="./github-page/static/img/dynasty.png" alt="./github-page/static/video/fam.mp4">
                 </div>
                 <div class="row">
-                    <img src="./static/img/invasion.png" alt="./static/video/fam.mp4">
+                    <img src="./github-page/static/img/invasion.png" alt="./github-page/static/video/fam.mp4">
                 </div>
                 <div class="row">
-                    <img src="./static/img/foundation.png" alt="./static/video/fam.mp4">
+                    <img src="./github-page/static/img/foundation.png" alt="./github-page/static/video/fam.mp4">
                 </div>
                 <div class="row">
-                    <img src="./static/img/moa.png" alt="./static/video/fam.mp4" >
+                    <img src="./github-page/static/img/moa.png" alt="./github-page/static/video/fam.mp4" >
                 </div>
             </div>
         </div>
