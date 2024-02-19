b446ca8cf00d83e80550da94cf14b9bc2286647f
Jaward Sesay <jkef96@icloud.com>
2024-02-19 18:31:33 +0800

    added app templates -todos

---
+++ github-page/static/js/apps/messages.js
@@ -0,0 +1,26 @@
+function appContent() {
+    return `
+    <div class="messages-content">
+    </div>
+    `
+}
+  
+function appControls() {
+    return `
+    
+          <!--- <div class="messages-sidebar">
+          </div> --->
+        `
+}
+  
+function bottomNav() {
+    return `
+    <!-- <div class="messages-controls">
+    </div> -->
+    `
+}
+  
+appContent();
+appControls();
+bottomNav();
+    
