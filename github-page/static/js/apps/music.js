86816c76d1b769dfb6837b3ff3f503e1ea04f29c
Jaward Sesay <jkef96@icloud.com>
2024-02-19 17:38:03 +0800

    add music template

---
+++ github-page/static/js/apps/music.js
@@ -0,0 +1,26 @@
+function appContent() {
+    return `
+    <div class="music-content">
+    </div>
+    `
+}
+  
+function appControls() {
+    return `
+    
+          <!--- <div class="music-sidebar">
+          </div> --->
+        `
+}
+  
+function bottomNav() {
+    return `
+    <!-- <div class="music-controls">
+    </div> -->
+    `
+}
+  
+appContent();
+appControls();
+bottomNav();
+    
