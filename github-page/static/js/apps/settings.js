aff9435a4eddcc1f716560e9a90396aa64126baa
Jaward Sesay <jkef96@icloud.com>
2024-02-19 17:36:55 +0800

    added settings template

---
+++ github-page/static/js/apps/settings.js
@@ -0,0 +1,26 @@
+function appContent() {
+    return `
+    <div class="settings-content">
+    </div>
+    `
+}
+  
+function appControls() {
+    return `
+    
+          <!--- <div class="settings-sidebar">
+          </div> --->
+        `
+}
+  
+function bottomNav() {
+    return `
+    <!-- <div class="settings-controls">
+    </div> -->
+    `
+}
+  
+appContent();
+appControls();
+bottomNav();
+    
